1763addd387ea376848be18e7117c88e119af908
Danilo <danilo.rocha1@uni9.edu.br>
2021-03-06 21:20:04 -0300

    First commit on backend codebase

---
+++ serverless-backend/lambdas/userHandlers/signup/app.js
@@ -0,0 +1,39 @@
+
+const bcrypt = require("bcryptjs");
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.lambdaHandler = async (event, context) => {
+    try {
+        const body = JSON.parse(event.body);
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(body.password, salt);
+        delete body.password;
+        body.hashedPassword = hash;
+        response = {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': JSON.stringify({
+                message: body,
+                env: process.env.MONGODB_URI
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+    return response
+};
